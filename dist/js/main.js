b3e0aab5e89ad768766b062322a3845cad6228dd
Alejandro Díaz Vecchio <aldiazve@gmail.com>
2017-06-04 12:56:16 -0500

    Added chart for hotels

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,17 +5,23 @@ function initGoogleMap() {
 	mapLib.initGoogleMap();
 }
 
+function showStatistics(){
+	
+}
+
 var main = {}
 
 main.initGoogleMap = initGoogleMap
+main.showStatistics = showStatistics
 
 window.main = main
 
 $(document).ready(function(){
       $('.carousel').carousel();
-			$('#restaurants').change( mapLib.filter.filterResta);
-			$('#museums').change(mapLib.filter.filterMuse);
-			$('#libraries').change( mapLib.filter.filterLib);
-			$('#culture_center').change( mapLib.filter.filterCul);
-			$('#theatres').change( mapLib.filter.filterThe);
+		$('#restaurants').change( mapLib.filter.filterResta);
+		$('#museums').change(mapLib.filter.filterMuse);
+		$('#libraries').change( mapLib.filter.filterLib);
+		$('#culture_center').change( mapLib.filter.filterCul);
+		$('#theatres').change( mapLib.filter.filterThe);
+		$('#statistics1').on('click', main.showStatistics);
 });
